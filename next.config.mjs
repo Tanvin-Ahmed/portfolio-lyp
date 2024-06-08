c263cdbca667cc6f4bd58643759dd80b37390521
Tanvin-Ahmed <tanvinahmedtouhid91@gmail.com>
2024-06-08 22:12:26 +0600

    add baseurl

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  basePath: isProduction ? "/portfolio-lyp" : "",
+  //   output: "export",
+  //   distDir: "dist",
+  //   images: {
+  //     unoptimized: true,
+  //   },
+};
 
 export default nextConfig;
